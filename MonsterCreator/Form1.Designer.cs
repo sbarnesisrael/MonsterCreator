8fa24dfcca17ad76e2f0ad6f55fd4f9ff24e9026
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2014-08-31 19:55:48 -0400

    Initial commit.

---
+++ MonsterCreator/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace MonsterCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUPC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtHP = new System.Windows.Forms.TextBox();
+            this.txtDefense = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtMP = new System.Windows.Forms.TextBox();
+            this.txtAgility = new System.Windows.Forms.TextBox();
+            this.txtBreed = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.txtLeadership = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPower = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UPC: ";
+            // 
+            // txtUPC
+            // 
+            this.txtUPC.Location = new System.Drawing.Point(78, 35);
+            this.txtUPC.Name = "txtUPC";
+            this.txtUPC.Size = new System.Drawing.Size(151, 20);
+            this.txtUPC.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "HP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Defense";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "MP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(234, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Agility";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(234, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Breed";
+            // 
+            // txtHP
+            // 
+            this.txtHP.Enabled = false;
+            this.txtHP.Location = new System.Drawing.Point(79, 145);
+            this.txtHP.Name = "txtHP";
+            this.txtHP.Size = new System.Drawing.Size(100, 20);
+            this.txtHP.TabIndex = 8;
+            // 
+            // txtDefense
+            // 
+            this.txtDefense.Enabled = false;
+            this.txtDefense.Location = new System.Drawing.Point(79, 178);
+            this.txtDefense.Name = "txtDefense";
+            this.txtDefense.Size = new System.Drawing.Size(100, 20);
+            this.txtDefense.TabIndex = 9;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Enabled = false;
+            this.txtGender.Location = new System.Drawing.Point(79, 246);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(100, 20);
+            this.txtGender.TabIndex = 10;
+            // 
+            // txtMP
+            // 
+            this.txtMP.Enabled = false;
+            this.txtMP.Location = new System.Drawing.Point(277, 145);
+            this.txtMP.Name = "txtMP";
+            this.txtMP.Size = new System.Drawing.Size(100, 20);
+            this.txtMP.TabIndex = 11;
+            // 
+            // txtAgility
+            // 
+            this.txtAgility.Enabled = false;
+            this.txtAgility.Location = new System.Drawing.Point(276, 178);
+            this.txtAgility.Name = "txtAgility";
+            this.txtAgility.Size = new System.Drawing.Size(100, 20);
+            this.txtAgility.TabIndex = 12;
+            // 
+            // txtBreed
+            // 
+            this.txtBreed.Enabled = false;
+            this.txtBreed.Location = new System.Drawing.Point(276, 242);
+            this.txtBreed.Name = "txtBreed";
+            this.txtBreed.Size = new System.Drawing.Size(100, 20);
+            this.txtBreed.TabIndex = 13;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(79, 359);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(194, 359);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 15;
+            this.btnScan.Text = "&Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtLeadership
+            // 
+            this.txtLeadership.Enabled = false;
+            this.txtLeadership.Location = new System.Drawing.Point(78, 211);
+            this.txtLeadership.Name = "txtLeadership";
+            this.txtLeadership.Size = new System.Drawing.Size(100, 20);
+            this.txtLeadership.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Leadership";
+            // 
+            // txtPower
+            // 
+            this.txtPower.Enabled = false;
+            this.txtPower.Location = new System.Drawing.Point(276, 211);
+            this.txtPower.Name = "txtPower";
+            this.txtPower.Size = new System.Drawing.Size(100, 20);
+            this.txtPower.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(234, 214);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Power";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 426);
+            this.Controls.Add(this.txtPower);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtLeadership);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtBreed);
+            this.Controls.Add(this.txtAgility);
+            this.Controls.Add(this.txtMP);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtDefense);
+            this.Controls.Add(this.txtHP);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUPC);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUPC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtHP;
+        private System.Windows.Forms.TextBox txtDefense;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtMP;
+        private System.Windows.Forms.TextBox txtAgility;
+        private System.Windows.Forms.TextBox txtBreed;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.TextBox txtLeadership;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPower;
+        private System.Windows.Forms.Label label9;
+    }
+}
