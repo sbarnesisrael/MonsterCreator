1ed6ce90edb19af7640c3c8c246621479f82c567
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2014-08-31 23:17:11 -0400

    Added breed enum with logic. Breed is now printed instead of number.

--- MonsterCreator/Form1.cs
+++ MonsterCreator/Form1.cs
@@ -36,10 +36,25 @@ namespace MonsterCreator
             txtLeadership.Text = "";
         }
 
+        enum MonstBreed
+        {
+            Animal = 0,
+            Plant = 10,
+            Fire = 20,
+            Water = 30,
+            Flying = 40,
+            Ground = 50,
+            Bug = 60,
+            Air = 70,
+            Light = 80,
+            Dark = 90,
+            Normal = 100
+        };
+
         private void button2_Click(object sender, EventArgs e)
         {
-            decimal UPC = decimal.Parse(txtUPC.Text);
-            Random rand = new Random();
+            double UPC = double.Parse(txtUPC.Text);
+            Random rand = new Random( (int) UPC );
 
             int rndHP = rand.Next(20);
             int rndMP = rand.Next(25);
@@ -50,12 +65,57 @@ namespace MonsterCreator
             int rndPower = rand.Next(50);
             int rndLeadership = rand.Next(55);
 
+            if (rndBreed >= 0 && rndBreed < 10)
+            {
+                txtBreed.Text = MonstBreed.Animal.ToString();
+            }
+            else if (rndBreed >= 10 && rndBreed < 20)
+            {
+                txtBreed.Text = MonstBreed.Plant.ToString();
+            }
+            else if (rndBreed >= 20 && rndBreed < 30)
+            {
+                txtBreed.Text = MonstBreed.Fire.ToString();
+            }
+            else if (rndBreed >= 30 && rndBreed < 40)
+            {
+                txtBreed.Text = MonstBreed.Water.ToString();
+            }
+            else if (rndBreed >= 40 && rndBreed < 50)
+            {
+                txtBreed.Text = MonstBreed.Flying.ToString();
+            }
+            else if (rndBreed >= 50 && rndBreed < 60)
+            {
+                txtBreed.Text = MonstBreed.Ground.ToString();
+            }
+            else if (rndBreed >= 60 && rndBreed < 70)
+            {
+                txtBreed.Text = MonstBreed.Bug.ToString();
+            }
+            else if (rndBreed >= 70 && rndBreed < 80)
+            {
+                txtBreed.Text = MonstBreed.Air.ToString();
+            }
+            else if (rndBreed >= 80 && rndBreed < 90)
+            {
+                txtBreed.Text = MonstBreed.Light.ToString();
+            }
+            else if (rndBreed >= 90)
+            {
+                txtBreed.Text = MonstBreed.Dark.ToString();
+            }
+            else
+            {
+                txtBreed.Text = MonstBreed.Normal.ToString();
+            }
+
             txtHP.Text = rndHP.ToString();
             txtMP.Text = rndMP.ToString();
             txtAgility.Text = rndAgility.ToString();
             txtDefense.Text = rndDefense.ToString();
             txtGender.Text = rndGender.ToString();
-            txtBreed.Text = rndBreed.ToString();
+            //txtBreed.Text = rndBreed.ToString();
             txtPower.Text = rndPower.ToString();
             txtLeadership.Text = rndLeadership.ToString();
             
